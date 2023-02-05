61a16d51ec870062a1ab877a65b2fa9e3a03f9e2
Gador Cruz <gadorcruz@hotmail.com>
2023-02-05 19:44:19 +0000

    add user validation and postcode column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :address, presence: true
+  validates :postcode, presence: true
   has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_05_192224) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_05_194027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_05_192224) do
     t.string "name"
     t.string "surname"
     t.text "address"
+    t.string "postcode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
